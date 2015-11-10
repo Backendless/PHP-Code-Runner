92e8b4829ee46004c34a5a9d04bfc343ea22a57c
Dmitry <dmitriy.matviychuk@themidnightcoders.com>
2015-11-10 16:42:01 +0200

    add https to host name

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
   'application_secret_key' => '<application_secret_key>',
   'application_version' => '<application_version>',
   'location_classes' => '../classes',
-  'system_server_url' => 'api.backendless.com',
+  'system_server_url' => 'https://api.backendless.com',
   'system_redis_host' => 'cl.backendless.com',
   'system_redis_port' => 6379,
   'system_pool_core' => 20,
