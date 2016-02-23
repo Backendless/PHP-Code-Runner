8698d59fca07b4c4bf9631805c05aafd0858bc88
Dmitry <dmitriy.matviychuk@themidnightcoders.com>
2016-02-23 18:28:06 +0200

    update sdk

--- lib/backendless/src/services/UserService.php
+++ lib/backendless/src/services/UserService.php
@@ -106,6 +106,12 @@ class UserService
     
     public function getCurrentUser() {
         
+        return $this->CurrentUser();
+        
+    }
+    
+    public function CurrentUser() {
+        
         if( isset( $this->current_user ) ) {
             
             return $this->current_user;
